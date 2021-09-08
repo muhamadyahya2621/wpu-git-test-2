37dcf9580b4aff4441f800844f9ef2e55bafd53b
muhamadyahya2621 <85600596+muhamadyahya2621@users.noreply.github.com>
2021-09-09 06:39:28 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut dolorum quam, fuga distinctio illo esse dolor tempora fugiat dicta ad, quo eius ipsa iure omnis corporis deleniti iste impedit ratione.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
